fd538fb3ba35959ae8ea6c74773c426694cf459d
marmot0814 <s1357andy871207@gmail.com>
2020-07-12 17:49:36 +0800

    Add comment input and display field

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -17,34 +17,61 @@ package com.google.sps.servlets;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
-@WebServlet("/data")
+@WebServlet("/comment")
 public class DataServlet extends HttpServlet {
 
-  private ArrayList<String> messages;
+  private ArrayList<String> usernames;
+  private ArrayList<String> contents;
+  private ArrayList<String> timestamps;
+
   public void init() {
-    messages = new ArrayList<String>();
-    messages.add("rock");
-    messages.add("paper");
-    messages.add("scissors");
+    usernames = new ArrayList<String>();
+    contents = new ArrayList<String>();
+    timestamps = new ArrayList<String>();
   }
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String json = convertToJson(messages);
+    String json = convertToJson(usernames, contents, timestamps);
     response.setContentType("application/json;");
     response.getWriter().println(json);
   }
-  private String convertToJson(ArrayList<String> messages) {
+  
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String username = request.getParameter("username");
+    String content = request.getParameter("content");
+
+    Date date = new Date();
+    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    String timestamp = formatter.format(date);
+
+    usernames.add(username);
+    contents.add(content);
+    timestamps.add(timestamp);
+
+    // Redirect back to the HTML page.
+    response.sendRedirect("/index.html");
+  }
+
+  private String convertToJson(ArrayList<String> usernames, ArrayList<String> contents, ArrayList<String> timestamps) {
     String json = "[";
-    for (int i = 0 ; i < messages.size() ; i++) {
-      json += "\"" + messages.get(i) + "\"";
-      if (i + 1 != messages.size())
+    int tot = usernames.size();
+    for (int i = 0 ; i < tot ; i++) {
+      json += "{";
+      json += "\"username\": \"" + usernames.get(i) + "\",";
+      json += "\"content\": \"" + contents.get(i) + "\",";
+      json += "\"timestamp\": \"" + timestamps.get(i) + "\"";
+      json += "}";
+      if (i + 1 != tot)
         json += ",";
     }
     json += "]";
