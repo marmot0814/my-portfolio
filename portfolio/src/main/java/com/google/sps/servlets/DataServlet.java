544c009dfd64754e43a6818dc681552ccf51b77d
marmot0814 <s1357andy871207@gmail.com>
2020-07-12 18:26:20 +0800

    Store the comment into database

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,10 +14,18 @@
 
 package com.google.sps.servlets;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.sps.data.Comment;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Date;
+import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -28,19 +36,24 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/comment")
 public class DataServlet extends HttpServlet {
 
-  private ArrayList<String> usernames;
-  private ArrayList<String> contents;
-  private ArrayList<String> timestamps;
-
-  public void init() {
-    usernames = new ArrayList<String>();
-    contents = new ArrayList<String>();
-    timestamps = new ArrayList<String>();
-  }
-
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String json = convertToJson(usernames, contents, timestamps);
+    Query query = new Query("Comment").addSort("timestamp", SortDirection.DESCENDING);
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+
+    ArrayList<Comment> comments = new ArrayList<>();
+    for (Entity entity : results.asIterable()) {
+      long id = entity.getKey().getId();
+      String username = (String) entity.getProperty("username");
+      String content = (String) entity.getProperty("content");
+      long timestamp = (long) entity.getProperty("timestamp");
+
+      Comment comment = new Comment(id, username, content, timestamp);
+      comments.add(comment);
+    }
+
+    String json = convertToJson(comments);
     response.setContentType("application/json;");
     response.getWriter().println(json);
   }
@@ -49,27 +62,32 @@ public class DataServlet extends HttpServlet {
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     String username = request.getParameter("username");
     String content = request.getParameter("content");
+    long timestamp = System.currentTimeMillis();
 
-    Date date = new Date();
-    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-    String timestamp = formatter.format(date);
+    Entity commentEntity = new Entity("Comment");
+    commentEntity.setProperty("username", username);
+    commentEntity.setProperty("content", content);
+    commentEntity.setProperty("timestamp", timestamp);
 
-    usernames.add(username);
-    contents.add(content);
-    timestamps.add(timestamp);
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(commentEntity);
 
     // Redirect back to the HTML page.
     response.sendRedirect("/index.html");
   }
-
-  private String convertToJson(ArrayList<String> usernames, ArrayList<String> contents, ArrayList<String> timestamps) {
+  private String timestampToString(long timestamp) {
+    Timestamp ts = new Timestamp(timestamp);
+    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    return formatter.format(ts);
+  }
+  private String convertToJson(ArrayList<Comment> comments) {
     String json = "[";
-    int tot = usernames.size();
+    int tot = comments.size();
     for (int i = 0 ; i < tot ; i++) {
       json += "{";
-      json += "\"username\": \"" + usernames.get(i) + "\",";
-      json += "\"content\": \"" + contents.get(i) + "\",";
-      json += "\"timestamp\": \"" + timestamps.get(i) + "\"";
+      json += "\"username\": \"" + comments.get(i).getUsername() + "\",";
+      json += "\"content\": \"" + comments.get(i).getContent() + "\",";
+      json += "\"timestamp\": \"" + timestampToString(comments.get(i).getTimestamp()) + "\"";
       json += "}";
       if (i + 1 != tot)
         json += ",";
