fdfc131432d0ce098f5c8ee080b51b220aa74a79
marmot0814 <s1357andy871207@gmail.com>
2020-07-19 00:10:33 +0800

    Implement query function and pass all tests

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,64 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.Iterator;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    
+    Set<String> attendees = new HashSet<String>(request.getAttendees());
+
+    Map<Integer, Integer> timeSlot = new TreeMap<Integer, Integer>();
+
+    for (Event event : events) {
+      TimeRange when = event.getWhen();
+
+      boolean hasRelatedPerson = false;
+      for (String attendee : event.getAttendees())
+        if (attendees.contains(attendee))
+          hasRelatedPerson = true;
+      
+      if (!hasRelatedPerson)
+        continue;
+
+      if (timeSlot.get(when.start()) == null) 
+        timeSlot.put(when.start(), 0);
+      timeSlot.put(when.start(), timeSlot.get(when.start()) + 1);
+
+      if (timeSlot.get(when.end()) == null)
+        timeSlot.put(when.end(), 0);
+      timeSlot.put(when.end(), timeSlot.get(when.end()) - 1);
+    }
+
+    ArrayList<TimeRange> availableTimeRanges = new ArrayList<>();
+    Integer sum = 0, prev = 0;
+
+    for(Map.Entry<Integer,Integer> entry : timeSlot.entrySet()) {
+      Integer key = entry.getKey();
+      Integer value = entry.getValue();
+
+      if (sum > 0 && sum + value == 0) {
+        // start a new empty time range
+        prev = key;
+      } else if (sum == 0 && sum + value > 0) {
+        // end a new empty time range
+        Integer duration = key - prev;
+        if (duration >= request.getDuration())
+          availableTimeRanges.add(TimeRange.fromStartDuration(prev, duration));
+      }
+      sum += value;
+    }
+
+    // tail
+    Integer duration = TimeRange.WHOLE_DAY.end() - prev;
+    if (duration >= request.getDuration())
+      availableTimeRanges.add(TimeRange.fromStartDuration(prev, duration));
+      
+    return availableTimeRanges;
   }
 }
