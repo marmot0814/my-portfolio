0ad0f1e81f5b82d675a2ddf45854251dc56f9064
marmot0814 <s1357andy871207@gmail.com>
2020-07-18 17:17:39 +0800

    Query comments order by timestamp

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,12 +14,17 @@
 
 package com.google.sps.servlets;
 
-import com.google.api.core.ApiFuture;
 import com.google.auth.oauth2.GoogleCredentials;
 
+import com.google.api.core.ApiFuture;
+import com.google.api.core.ApiFutures;
+import com.google.cloud.firestore.CollectionReference;
 import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.FirestoreOptions;
+import com.google.cloud.firestore.Query;
+import com.google.cloud.firestore.Query.Direction;
 import com.google.cloud.firestore.QueryDocumentSnapshot;
 import com.google.cloud.firestore.QuerySnapshot;
 import com.google.cloud.firestore.WriteResult;
@@ -53,11 +58,13 @@ public class DataServlet extends HttpServlet {
         .build().getService();
 
     try {
-      List<QueryDocumentSnapshot> documents = 
-        db.collection("comments").get().get().getDocuments();
+      ApiFuture<QuerySnapshot> querySnapshot = 
+        db.collection("comments")
+          .orderBy("timestamp", Direction.DESCENDING)
+          .get();
 
       ArrayList<Comment> comments = new ArrayList<>();
-      for (QueryDocumentSnapshot document : documents) {
+      for (QueryDocumentSnapshot document : querySnapshot.get().getDocuments()) {
         Comment comment = new Comment(
           document.getId(),
           document.getString("username"),
@@ -79,28 +86,30 @@ public class DataServlet extends HttpServlet {
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-    Firestore db = 
-      FirestoreOptions.getDefaultInstance().toBuilder()
-        .setProjectId("zyang-sps-summer20")
-        .setCredentials(GoogleCredentials.getApplicationDefault())
-        .build().getService();
-    DocumentReference docRef = db.collection("comments").document();
-
     Map<String, Object> data = new HashMap<>();
     data.put("username", request.getParameter("username"));
     data.put("content", request.getParameter("content"));
     data.put("timestamp", System.currentTimeMillis());
 
+    Firestore db = 
+      FirestoreOptions.getDefaultInstance().toBuilder()
+        .setProjectId("zyang-sps-summer20")
+        .setCredentials(GoogleCredentials.getApplicationDefault())
+        .build().getService();
+
+    DocumentReference docRef = db.collection("comments").document();
     ApiFuture<WriteResult> result = docRef.set(data);
 
     // Redirect back to the HTML page.
     response.sendRedirect("/index.html");
   }
+
   private String timestampToString(long timestamp) {
     Timestamp ts = new Timestamp(timestamp);
     SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     return formatter.format(ts);
   }
+
   private String convertToJson(ArrayList<Comment> comments) {
     String json = "[";
     int tot = comments.size();
